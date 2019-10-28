c3ab29d7b2e6b4a57cf1ee246b4b2e99b658b227
Mark Howard <bark4mark@gmail.com>
2019-10-28 19:35:35 +0000

    Updates

---
+++ precache-manifest.5b640c889f0ca0e608dfc783f938968c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4ce4f614440efbdad8938d1cb7ee6f",
+    "url": "/dash/index.html"
+  },
+  {
+    "revision": "c7d543a0e9d1193cefe2",
+    "url": "/dash/static/css/main.fbccc0ac.chunk.css"
+  },
+  {
+    "revision": "40a046b3cbd2a016c200",
+    "url": "/dash/static/js/2.0445589e.chunk.js"
+  },
+  {
+    "revision": "c7d543a0e9d1193cefe2",
+    "url": "/dash/static/js/main.8b27c4e8.chunk.js"
+  },
+  {
+    "revision": "27d45cbe6a474347b7d0",
+    "url": "/dash/static/js/runtime-main.cc6824a4.js"
+  }
+]);
