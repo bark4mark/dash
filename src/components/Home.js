660cd9378758d8aa28c7b4db9a96e34e76e98da5
Mark Howard <bark4mark@gmail.com>
2019-10-31 20:54:22 +0000

    Adding styles for side menu and starting on close button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,30 @@ import Header from './header/Header';
 import SideNav from './navigation/SideNav';
 import Main from './main/Main';
 import Footer from './footer/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import '../css/reset.css';
 import '../css/main.css';
 
+
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {active:false};
+  }
+  toggleClass = () => {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
   render(){
     return (
       <div className="grid-container">
+        <div className="menu-icon">
+          <FontAwesomeIcon onClick={this.toggleClass} icon={faBars} />
+        </div>
         <Header />
-        <SideNav />
+        <SideNav active={this.state.active} />
         <Main />
         <Footer />
       </div>
--- src/components/navigation/SideNav.js
+++ src/components/navigation/SideNav.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import SideNavItem from './SideNavItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 class SideNav extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      active: props.active
+    }
+  }
+  toggleClass = () => {
+    const currentState = this.active;
+    this.setState({ active: !currentState });
+  }
   render () {
     return (
-      <aside className="sidenav">
+      <aside className={this.state.active ? 'sidenav active': 'sidenav'}>
+        <div className="sidenav__close-icon">
+          <FontAwesomeIcon onClick={this.toggleClass} icon={faTimes} />
+        </div>
         <ul className="sidenav__list">
           <SideNavItem item="Home"/>
           <SideNavItem item="About"/>
